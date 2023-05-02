9e52f15d6fda68c24dbb86c518123e898be153f9
Ashley <u2789011@gmail.com>
2023-05-02 16:45:43 +0800

    init

---
+++ src/pages/Admin.js
@@ -0,0 +1,113 @@
+import { useState } from "react";
+import { Button, Flex, Heading, Text, useToast } from "@chakra-ui/react";
+
+function Admin({ isOwner, connectedContract }) {
+  const toast = useToast();
+  const [openSaleTxnPending, setOpenSaleTxnPending] = useState(false);
+
+  const [closeSaleTxnPending, setCloseSaleTxnPending] = useState(false);
+
+  const closeSale = async () => {
+    try {
+      if (!connectedContract) return;
+
+      setCloseSaleTxnPending(true);
+      let closeSaleTxn = await connectedContract.closeSale();
+
+      await closeSaleTxn.wait();
+      setCloseSaleTxnPending(false);
+
+      toast({
+        status: "success",
+        title: "Sale is closed!",
+        variant: "subtle",
+        description: (
+          <a
+            href={`https://goerli.etherscan.io/tx/${closeSaleTxn.hash}`}
+            target="_blank"
+            rel="nofollow noreferrer"
+          >
+            Checkout the transaction on Etherscan
+          </a>
+        ),
+      });
+    } catch (error) {
+      console.log(error);
+      setCloseSaleTxnPending(true);
+      toast({
+        title: "Failure",
+        description: error,
+        status: "error",
+        variant: "subtle",
+      });
+    }
+  };
+
+  const openSale = async () => {
+    try {
+      if (!connectedContract) return;
+
+      setOpenSaleTxnPending(true);
+      let openSaleTxn = await connectedContract.openSale();
+
+      await openSaleTxn.wait();
+      setOpenSaleTxnPending(false);
+
+      toast({
+        status: "success",
+        title: "Sale is open!",
+        variant: "subtle",
+        description: (
+          <a
+            href={`https://goerli.etherscan.io/tx/${openSaleTxn.hash}`}
+            target="_blank"
+            rel="nofollow noreferrer"
+          >
+            Checkout the transaction on Etherscan
+          </a>
+        ),
+      });
+    } catch (error) {
+      console.log(error);
+      setOpenSaleTxnPending(false);
+      toast({
+        title: "Failure",
+        description: error,
+        status: "error",
+        variant: "subtle",
+      });
+    }
+  };
+  return (
+    <>
+      <Heading mb={4}>Admin panel</Heading>
+      <Text fontSize="xl" mb={8}>
+        Enable and disable sales on the smart contract.
+      </Text>
+      <Flex width="100%" justifyContent="center">
+        <Button
+          isLoading={openSaleTxnPending}
+          onClick={openSale}
+          isDisabled={!isOwner || closeSaleTxnPending}
+          size="lg"
+          colorScheme="teal"
+        >
+          Open Sale
+        </Button>
+        <Button
+          onClick={closeSale}
+          isLoading={closeSaleTxnPending}
+          isDisabled={!isOwner || openSaleTxnPending}
+          size="lg"
+          colorScheme="red"
+          variant="solid"
+          marginLeft="24px"
+        >
+          Close Sale
+        </Button>
+      </Flex>
+    </>
+  );
+}
+
+export default Admin;
---
+++ src/pages/Buy.js
@@ -0,0 +1,115 @@
+import { useEffect, useState } from "react";
+import {
+  Button,
+  ButtonGroup,
+  Heading,
+  Text,
+  Flex,
+  useToast,
+} from "@chakra-ui/react";
+
+function Buy({ connectedContract }) {
+  const toast = useToast();
+  const [totalTicketCount, setTotalTicketCount] = useState(null);
+
+  const [availableTicketCount, setAvailableTicketCount] = useState(null);
+
+  const [buyTxnPending, setBuyTxnPending] = useState(false);
+
+  useEffect(() => {
+    if (!connectedContract) return;
+
+    getAvailableTicketCount();
+    getTotalTicketCount();
+  });
+
+  const buyTicket = async () => {
+    try {
+      if (!connectedContract) return;
+
+      setBuyTxnPending(true);
+      const buyTxn = await connectedContract.mint({
+        value: `${0.08 * 10 ** 18}`,
+      });
+
+      await buyTxn.wait();
+      setBuyTxnPending(false);
+      toast({
+        title: "Success!",
+        description: (
+          <a
+            href={`https://goerli.etherscan.io/tx/${buyTxn.hash}`}
+            target="_blank"
+            rel="nofollow noreferrer"
+          >
+            Checkout the transaction on Etherscan
+          </a>
+        ),
+        status: "success",
+        variant: "subtle",
+      });
+    } catch (err) {
+      console.log(err);
+      setBuyTxnPending(false);
+      toast({
+        title: "Failed.",
+        description: err,
+        status: "error",
+        variant: "subtle",
+      });
+    }
+  };
+
+  const getAvailableTicketCount = async () => {
+    try {
+      const count = await connectedContract.availableTicketCount();
+      setAvailableTicketCount(count.toNumber());
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  const getTotalTicketCount = async () => {
+    try {
+      const count = await connectedContract.totalTicketCount();
+      setTotalTicketCount(count.toNumber());
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  return (
+    <>
+      <Heading mb={4}>DevDAO Conference 2022</Heading>
+      <Text fontSize="xl" mb={4}>
+        Connect your wallet to mint your NFT. It'll be your ticket to get in!
+      </Text>
+      <Flex
+        flexDirection="column"
+        alignItems="center"
+        justifyContent="center"
+        margin="0 auto"
+        maxW="140px"
+      >
+        <ButtonGroup mb={4}>
+          <Button
+            onClick={buyTicket}
+            isLoading={buyTxnPending}
+            loadingText="Pending"
+            size="lg"
+            colorScheme="teal"
+          >
+            Buy Ticket
+          </Button>
+        </ButtonGroup>
+        {availableTicketCount && totalTicketCount && (
+          <Text>
+            {availableTicketCount} of {totalTicketCount} minted!
+          </Text>
+        )}
+      </Flex>
+    </>
+  );
+}
+
+export default Buy;

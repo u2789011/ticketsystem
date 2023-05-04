2906b7b17d8df007e3e983c1d6a1e0b50e4b807f
Ashley <u2789011@gmail.com>
2023-05-04 15:28:43 +0800

    fix camera direction

--- src/pages/CheckIn.js
+++ src/pages/CheckIn.js
@@ -117,7 +117,7 @@ function CheckIn({ connectedContract }) {
           <Box margin="16px auto 8px auto" padding="0 16px" width="360px">
             <QrReader
               delay={3000}
-              facingMode="rear"
+              facingMode={"environment"}
               style={{
                 maxWidth: "100%",
                 margin: "0 auto",

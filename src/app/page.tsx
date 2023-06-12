ea69a7a632791a1ce24df012d6055fad82feaaa5
Chen-Hsien <zxcvb1088@gmail.com>
2023-06-12 11:58:50 +0800

    [Angus] Add Zone select & rewrite get ticket FC

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,44 +6,90 @@ import {
   Heading,
   Text,
   Flex,
+  Select,
 } from "@chakra-ui/react";
 import { HomeContext } from "./home";
 import useCustomToast from "../../components/hooks/useCustomToast";
 
-
 const Buy = () => {
   const context = useContext(HomeContext);
   if (context === undefined) {
-    throw new Error('useContext undefined')
+    throw new Error("useContext undefined");
   }
-  console.log("BUY, context", context)
+  console.log("BUY, context", context);
   const { connectedContract } = context;
   const { showSuccessToastWithReactNode, showErrorToast, showWarningToast } = useCustomToast();
-  const [totalTicketCount, setTotalTicketCount] = useState<number|null>(null);
-  const [availableTicketCountA, setAvailableTicketCountA] = useState<number|null>(null);
-  const [availableTicketCountB, setAvailableTicketCountB] = useState<number|null>(null);
-  const [availableTicketCountC, setAvailableTicketCountC] = useState<number|null>(null);
-
+  const [totalTicketCount, setTotalTicketCount] = useState<number | null | undefined>(null);
+  const [availableTicketCountA, setAvailableTicketCountA] = useState< number | null | undefined>(null);
+  const [availableTicketCountB, setAvailableTicketCountB] = useState< number | null | undefined>(null);
+  const [availableTicketCountC, setAvailableTicketCountC] = useState< number | null | undefined>(null);
+  const [currentOption, setCurrentOption] = useState<string | null>(null);
   const [buyTxnPending, setBuyTxnPending] = useState(false);
+
 
   useEffect(() => {
     if (!connectedContract) return;
+  
+    (async () => {
+      const [availableA, availableB, availableC] = await Promise.all([
+        fetchAvailableTicketCount('A'),
+        fetchAvailableTicketCount('B'),
+        fetchAvailableTicketCount('C'),
+        getTotalTicketCount(),
+      ]);
+  
+      setAvailableTicketCountA(availableA);
+      setAvailableTicketCountB(availableB);
+      setAvailableTicketCountC(availableC);
+    })();
+  
+  }, [connectedContract]);
+  
+  const fetchAvailableTicketCount = async (ticketType : string) => {
+    try {
+      const count = await connectedContract?.[`availableTickets${ticketType}`]();
+      if (typeof count === "bigint") {
+        return Number(count);
+      } else {
+        console.error(`Unexpected result from availableTickets${ticketType}: `, count);
+      }
+    } catch (err: any) {
+      console.error(err);
+      return null;
+    }
+  };
 
-    getAvailableTicketCountA();
-    getAvailableTicketCountB();
-    getAvailableTicketCountC();
-    getTotalTicketCount();
-    console.log(availableTicketCountA, availableTicketCountB, availableTicketCountC, totalTicketCount)
-  });
+  const getTotalTicketCount = async () => {
+    try {
+      const count = await connectedContract?.TOTAL_TICKETS();
+      console.log("connectedContract.TOTAL_TICKETS()", count);
+      setTotalTicketCount(Number(count));
+    } catch (err) {
+      console.log(err);
+    }
+  };
 
   const buyTicket = async () => {
     try {
       if (!connectedContract) return;
 
       setBuyTxnPending(true);
-      const buyTxn = await connectedContract.mintA({
-        value: `${0.001 * 10 ** 18}`,
-      });
+      let buyTxn;
+      if (currentOption === "option1") {
+        buyTxn = await connectedContract.mintA({
+          value: `${0.001 * 10 ** 18}`,
+        });
+      } else if (currentOption === "option2") {
+        buyTxn = await connectedContract.mintB({
+          value: `${0.002 * 10 ** 18}`,
+        });
+      } else if (currentOption === "option3") {
+        buyTxn = await connectedContract.mintC({
+          value: `${0.003 * 10 ** 18}`,
+        });
+      } else {
+        throw new Error("Invalid option");
+      }
 
       await buyTxn.wait();
       setBuyTxnPending(false);
@@ -57,83 +103,21 @@ const Buy = () => {
           在區塊鏈瀏覽器確認交易！
         </a>
       );
-    } catch (err:any) {
+    } catch (err: any) {
       console.log("buyTxn", err);
       console.log("buyTxn", err.code);
-        if (err?.code === "ACTION_REJECTED") {
-          showWarningToast(
-            "使用者拒絕簽署交易",
-            "請同意簽署本次購買票券之交易"
-          );
-        } else if (err.code === "INSUFFICIENT_FUNDS") {
-          showWarningToast(
-            "餘額不足",
-            "請加值您的錢包餘額"
-          );
-        } else {
-          showErrorToast(
-            "錯誤",
-            "交易錯誤, 請通知系統管理員."
-          );
-        }
+      if (err?.code === "ACTION_REJECTED") {
+        showWarningToast("使用者拒絕簽署交易", "請同意簽署本次購買票券之交易");
+      } else if (err.code === "INSUFFICIENT_FUNDS") {
+        showWarningToast("餘額不足", "請加值您的錢包餘額");
+      } else {
+        showErrorToast("錯誤", "交易錯誤, 請通知系統管理員.");
+      }
       setBuyTxnPending(false);
     }
   };
 
-  const getAvailableTicketCountA = async () => {
-    try {
-      const count = await connectedContract?.availableTicketsA();
-  
-      if (typeof count === 'bigint') {
-        setAvailableTicketCountA(Number(count));
-        console.log("connectedContract.availableTicketsA()", count);
-      } else {
-        console.error("Unexpected result from availableTicketsA: ", count);
-      }
-    } catch (err: any) {
-      console.error(err);
-    }
-  };
-  const getAvailableTicketCountB = async () => {
-    try {
-      const count = await connectedContract?.availableTicketsB();
-  
-      if (typeof count === 'bigint') {
-        setAvailableTicketCountB(Number(count));
-        console.log("connectedContract.availableTicketsB()", count);
-      } else {
-        console.error("Unexpected result from availableTicketsB: ", count);
-      }
-    } catch (err: any) {
-      console.error(err);
-    }
-  };
 
-  const getAvailableTicketCountC = async () => {
-    try {
-      const count = await connectedContract?.availableTicketsC();
-  
-      if (typeof count === 'bigint') {
-        setAvailableTicketCountC(Number(count));
-        console.log("connectedContract.availableTicketsC()", count);
-      } else {
-        console.error("Unexpected result from availableTicketsC: ", count);
-      }
-    } catch (err: any) {
-      console.error(err);
-      // 在此處進行錯誤處理，例如設置一個狀態以顯示錯誤消息
-    }
-  };
-
-  const getTotalTicketCount = async () => {
-    try {
-      const count = await connectedContract?.TOTAL_TICKETS();
-      console.log("connectedContract.TOTAL_TICKETS()",count)
-      setTotalTicketCount(Number(count));
-    } catch (err) {
-      console.log(err);
-    }
-  };
 
   return (
     <>
@@ -148,6 +132,17 @@ const Buy = () => {
         margin="0 auto"
         maxW="140px"
       >
+        <ButtonGroup mb={4} size="lg">
+          <Select
+            placeholder="選擇票種"
+            size="lg"
+            onChange={(e) => setCurrentOption(e.target.value)}
+          >
+            <option value="option1">Ａ區</option>
+            <option value="option2">Ｂ區</option>
+            <option value="option3">Ｃ區</option>
+          </Select>
+        </ButtonGroup>
         <ButtonGroup mb={4}>
           <Button
             onClick={buyTicket}
@@ -159,7 +154,6 @@ const Buy = () => {
           >
             購買票券
           </Button>
-
         </ButtonGroup>
         {totalTicketCount && (
           <Text>

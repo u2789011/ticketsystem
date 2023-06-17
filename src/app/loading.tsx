746b0fb2dcc26c068ac54317ca9fbca71bf34a41
Gary Lin 林品辰 <garylin2018@gmail.com>
2023-06-17 10:05:18 +0800

    Add flex to loading.tsx

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -1,16 +1,18 @@
 "use client";
 
 import React from "react";
-import { CircularProgress } from "@chakra-ui/react";
+import { CircularProgress, Flex } from "@chakra-ui/react";
 
 function Loading() {
   return (
-    <CircularProgress
-      capIsRound
-      isIndeterminate
-      color="green.300"
-      size="120px"
-    />
+    <Flex justifyContent="center" width="100%" marginTop="20%">
+      <CircularProgress
+        capIsRound
+        isIndeterminate
+        color="green.300"
+        size="120px"
+      />
+    </Flex>
   );
 }
 

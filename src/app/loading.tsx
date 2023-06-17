681bb1a6ec11cee70667dec7631449fb833060de
Gary Lin 林品辰 <garylin2018@gmail.com>
2023-06-17 08:59:28 +0800

    Add circular progress to loading page

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -1,7 +1,17 @@
+"use client";
+
 import React from "react";
+import { CircularProgress } from "@chakra-ui/react";
 
 function Loading() {
-  return <div>Loading</div>;
+  return (
+    <CircularProgress
+      capIsRound
+      isIndeterminate
+      color="green.300"
+      size="120px"
+    />
+  );
 }
 
 export default Loading;

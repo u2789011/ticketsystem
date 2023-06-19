d59e3cd787d65ca828581831f213d4db173c0c5d
Gary Lin 林品辰 <garylin2018@gmail.com>
2023-06-19 11:05:50 +0800

    Add useIsMounted hook to fix hydration error

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,6 +14,7 @@ import useCustomToast from "../../components/hooks/useCustomToast";
 import { useContractReads } from "wagmi";
 import { nfTixBooth } from "../../contracts/abis/nfTixBooth";
 import { usePrepareContractWrite, useContractWrite } from "wagmi";
+import { useIsMounted } from "./hooks/useIsMounted";
 
 const Buy = () => {
   const context = useContext(HomeContext);
@@ -24,21 +25,13 @@ const Buy = () => {
   const { connectedContract } = context;
   const { showSuccessToastWithReactNode, showErrorToast, showWarningToast } =
     useCustomToast();
-  // const [totalTicketCount, setTotalTicketCount] = useState<
-  //   number | null | undefined
-  // >(null);
-  // const [availableTicketCountA, setAvailableTicketCountA] = useState<
-  //   number | null | undefined
-  // >(null);
-  // const [availableTicketCountB, setAvailableTicketCountB] = useState<
-  //   number | null | undefined
-  // >(null);
-  // const [availableTicketCountC, setAvailableTicketCountC] = useState<
-  //   number | null | undefined
-  // >(null);
   const [currentOption, setCurrentOption] = useState<string | null>(null);
   const [buyTxnPending, setBuyTxnPending] = useState<boolean>(false);
 
+  // Check if mounted to deal with hydration error
+  const mounted = useIsMounted();
+
+  // Wagmi contract reads for available tickets
   const {
     data: availableTickets,
     isError,
@@ -69,61 +62,6 @@ const Buy = () => {
   });
 
   console.log(availableTickets);
-
-  // const { data, isError, isLoading } = useContractRead({
-  //   address: process.env.NEXT_PUBLIC_CONTRACT_ID as `0x${string}`,
-  //   abi: nfTixBooth.abi,
-  //   functionName: `availableTicketsA`,
-  // });
-  // console.log("DATA:", Number(data));
-
-  // useEffect(() => {
-  //   if (!connectedContract) return;
-
-  //   (async () => {
-  //     const [availableA, availableB, availableC] = await Promise.all([
-  //       fetchAvailableTicketCount("A"),
-  //       fetchAvailableTicketCount("B"),
-  //       fetchAvailableTicketCount("C"),
-
-  //       getTotalTicketCount(),
-  //     ]);
-
-  //     setAvailableTicketCountA(availableA);
-  //     setAvailableTicketCountB(availableB);
-  //     setAvailableTicketCountC(availableC);
-  //   })();
-
-  // }, [connectedContract]);
-
-  // const fetchAvailableTicketCount = async (ticketType: string) => {
-  //   try {
-  //     const count = await connectedContract?.[
-  //       `availableTickets${ticketType}`
-  //     ]();
-  //     if (typeof count === "bigint") {
-  //       return Number(count);
-  //     } else {
-  //       console.error(
-  //         `Unexpected result from availableTickets${ticketType}: `,
-  //         count
-  //       );
-  //     }
-  //   } catch (err: any) {
-  //     console.error(err);
-  //     return null;
-  //   }
-  // };
-
-  // const getTotalTicketCount = async () => {
-  //   try {
-  //     const count = await connectedContract?.TOTAL_TICKETS();
-  //     console.log("connectedContract.TOTAL_TICKETS()", count);
-  //     setTotalTicketCount(Number(count));
-  //   } catch (err) {
-  //     console.log(err);
-  //   }
-  // };
 
   // Configure Buy Ticket Writes
   const { config: configBuyA, error: errorBuyA } = usePrepareContractWrite({
@@ -158,21 +96,12 @@ const Buy = () => {
       setBuyTxnPending(true);
       let buyTxn;
       if (currentOption === "option1") {
-        // buyTxn = await connectedContract.mintA({
-        //   value: `${0.001 * 10 ** 18}`,
-        // });
         buyA?.();
         buyTxn = dataA?.hash;
       } else if (currentOption === "option2") {
-        // buyTxn = await connectedContract.mintB({
-        //   value: `${0.002 * 10 ** 18}`,
-        // });
         buyB?.();
         buyTxn = dataB?.hash;
       } else if (currentOption === "option3") {
-        // buyTxn = await connectedContract.mintC({
-        //   value: `${0.003 * 10 ** 18}`,
-        // });
         buyC?.();
         buyTxn = dataC?.hash;
       } else {
@@ -183,7 +112,7 @@ const Buy = () => {
       showSuccessToastWithReactNode(
         "成功",
         <a
-          href={`https://mumbai.polygonscan.com/tx/${buyTxn}`}
+          href={mounted ? `https://mumbai.polygonscan.com/tx/${buyTxn}` : ""}
           target="_blank"
           rel="nofollow noreferrer"
         >
@@ -232,7 +161,7 @@ const Buy = () => {
         </ButtonGroup>
         <ButtonGroup mb={4}>
           <Button
-            onClick={buyTicket}
+            onClick={mounted ? buyTicket : undefined}
             isLoading={buyTxnPending}
             loadingText="Pending"
             size="lg"
@@ -242,18 +171,7 @@ const Buy = () => {
             購買票券
           </Button>
         </ButtonGroup>
-        {/* {totalTicketCount && (
-          <Text>
-            總共 {totalTicketCount} 張票
-            <br />
-            A區還剩 {availableTicketCountA} 張！
-            <br />
-            B區還剩 {availableTicketCountB} 張！
-            <br />
-            C區還剩 {availableTicketCountC} 張！
-          </Text>
-        )} */}
-        {availableTickets && (
+        {mounted && availableTickets && (
           <Text>
             總共 {Number(availableTickets[0].result)} 張票
             <br />
